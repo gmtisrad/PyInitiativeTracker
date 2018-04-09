1ca7df2d1593a7f4de91608ffeb9e71bf85f2a9e
= <gabe.m.timm@gmail.com>
2018-04-09 01:46:35 -0700

    Added dex mod. Changed wording. Funcationality turned into functions.

--- InitiativeTracker.py
+++ InitiativeTracker.py
@@ -2,25 +2,41 @@ import operator
 import random
 import os
 
+#****Class Definitions****
 #Abstract Base Class for game entities
 class entity():
     name = None
     initiative = None
 
-#Implementation of entity for player objects
+#Implementation of subclass of entity for player objects
 class player(entity):
     def __init__(self):
-        self.name = input ("Enter name: ")
+        self.get_name()
+        self.get_initiative()
+    def get_name(self):
+        self.name = input ("Enter player name: ")
     def get_initiative(self):
-        self.initiative = int (input("Enter %s's initiative: " % (self.name)))
+        self.initiative = int(input("Enter %s's initiative: " % (self.name)))
 
-#Implementation of entity for monster/creature objects
+#Implementation of subclass of entity for monster/creature
 class monster(entity):
     def __init__(self, monster_count):
-        self.initiative = int(random.randint(1, 20))
-        self.name = ("Monster%d" % (monster_count))
+        self.set_name(monster_count)
+        self.get_dex_mod()
+        self.set_initiative()
+
+    def get_dex_mod(self, monster_count):
+        self.dex_mod = int(input("Enter monster group %d's dexterity modifier: "))
+
+    def set_initiative(self):
+        self.initiative = int(random.randint(1, 20) + dex_mod)
+
+    def set_name(self, monster_count):
+        self.name = ("Monster Group %d" % (monster_count))
+
     dex_mod = 0
 
+#****Start Program****
 new_game = None
 
 os.system ('cls')
@@ -34,7 +50,7 @@ players = []
 #Populates player list with initialized objects to be stored
 for i in range(0, number_of_players):
   players.append(player())
-
+#Begin program loop
 while (new_game != 'Q' or new_game != 'q'):
     monsters = []
     entities = []
@@ -50,7 +66,7 @@ while (new_game != 'Q' or new_game != 'q'):
     for i in players:
         i.get_initiative()
 
-    #Compiles entities into one list and sorts them in descending initiative order
+    #Compiles entities into one list and sorts them in descending (reversed) initiative order
     entities = players + monsters
     sorted_entities = sorted(entities, reverse = True, key=operator.attrgetter("initiative"))
 
